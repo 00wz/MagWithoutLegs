69a02605134254280740c9d4abb76b3f27519b12
Pavel <pedaracha@gmail.com>
2025-09-28 13:17:19 +0300

    Initial commit

---
+++ Assets/Scripts/Interfaces/IDamageable.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IDamageable
+{
+    TakeHealth(float value);
+
+    float GetHealth();
+}
---
+++ Assets/Scripts/Interfaces/IHUD.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public interface IHUD
+{
+    SetHealth(float value);
+
+    SetHealCooldown(float value);
+
+    SetFireballCooldown(float value);
+
+    SetTargetHealth(float value);
+}
